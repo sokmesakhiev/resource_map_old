00b972fb1361466e75987606b6cbcdedb51189d7
DaneSavot <devnull@localhost>
2013-07-05 14:47:44 +0700

    fixed conflict stylesheet on collection page

--- app/assets/javascripts/mobile.js
+++
@@ -1,8 +0,0 @@
-//= require jquery
-//= require jquery_ujs
-//= require knockout-2.2.1
-//= require custom_bindings/mobileenable
-//= require mobile/mobilecache
-//= require mobile/collections
-//= require mobile/events
-//= require jquery.mobile-1.3.1.min
