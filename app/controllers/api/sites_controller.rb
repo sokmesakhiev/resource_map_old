707548859239391db190af2be7bea3faf9e7ed7c
sreng <sreng@instedd.org>
2013-07-18 10:44:33 +0700

    build api for creating site

--- app/controllers/api/sites_controller.rb
+++ app/controllers/api/sites_controller.rb
@@ -37,4 +37,42 @@ class Api::SitesController < ApplicationController
       render json: site.errors.messages, status: :unprocessable_entity, :layout => false
     end
   end
+
+  def destroy
+    site = Site.find_by_id(params[:id])
+    site.destroy
+    render json: {site: site}
+  end
+
+  def create
+    site_params = {}
+    properties = prepare_site_property params
+    site_params.merge!("name" => params[:name])
+    site_params.merge!("lat" => params[:lat])
+    site_params.merge!("lng" => params[:lng])
+    site_params.merge!("properties" => properties)
+    current_user = User.find_by_email(params[:email])
+    collection = Collection.find_by_id(params[:collection_id])
+    site = collection.sites.new(site_params.merge!(user: current_user))
+    if site.valid?
+      site.save!
+      # Site::UploadUtils.uploadFile(params[:fileUpload])
+      current_user.site_count += 1
+      current_user.update_successful_outcome_status
+      current_user.save!
+      render json: {site: site, status: 201}
+    end
+  end
+
+  def prepare_site_property params
+    properties = {}
+    conflict_state_id = Field.find_by_code("con_state").id.to_s
+    conflict_type_id = Field.find_by_code("con_type").id.to_s
+    conflict_intensity_id = Field.find_by_code("con_intensity").id.to_s
+    properties.merge!(conflict_state_id => params[:conflict_state])
+    properties.merge!(conflict_type_id => params[:conflict_type])
+    properties.merge!(conflict_intensity_id => params[:conflict_intensity])
+
+    return properties
+  end
 end
