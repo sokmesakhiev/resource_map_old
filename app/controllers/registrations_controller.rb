59515d4b531b3bb59495a49ae92ddedd442dea88
DaneSavot <devnull@localhost>
2013-08-16 10:32:36 +0700

    Consider phone_number and phone_number with plus as the same one

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,12 +5,14 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def create
+    params['user']['phone_number'].delete!('+')
     super
   end
 
   def update
     email_changed = current_user.email != params[:user][:email]
     password_changed = !params[:user][:password].empty?
+    params['user']['phone_number'].delete!('+')
     successfully_updated = if email_changed or password_changed
       current_user.update_with_password(params[:user])
     else
