99288b636c3bbb981ad4c3d79bdb6f9fa2111641
Reaksmey <devnull@localhost>
2013-11-13 17:26:44 +0700

    change raw message look

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -30,74 +30,31 @@ def index
 			all_messages = []
 			size = 25
 
-			users.each do |user|
-				tmps = Message.order('id desc').where('`from` like ?', "%#{user.phone_number}%")
-				tmps.each do |t|
-					all_messages.push(t)
-				end
-			end
+			collections_id.push(nil)
 
-
-			# msgs = Message.order('id desc').where(collection_id: collections_id)
-			# msgs = msgs.limit(5)
-			# msgs = msgs.where('id < ?', params[:before_id]) if params[:before_id]
-
-
-			if params[:user_phone_numbers]
-				msgs = []
-				phone_messages = []
-				params[:user_phone_numbers].each do |phone_number|
-					if phone_number != "0"
-						tmp_msgs = Message.order('id desc').where('`from` like ?', "%#{phone_number}%")
-						tmp_msgs.each do |msg|
-							phone_messages.push(msg)
-						end
-					end
-				end
-			end
-
-
-			if params[:collection_ids]
-				msgs = []
-				msgs_collection = []
-				if params[:collection_ids] == ["0"]
-					msgs_collection = Message.order('id desc').where(:collection_id => nil)
+			if params[:raws] || params[:collection_ids]
+				if params[:raws] == ["0"] && !params[:collection_ids]
+					collections_id = current_user.memberships.pluck(:collection_id)
+				elsif params[:raws] != ["0"] && !params[:collection_ids]
+					collections_id = current_user.memberships.pluck(:collection_id).push(nil)
+				elsif params[:raws] == ["0"] && params[:collection_ids] != 0
+					collections_id = params[:collection_ids]
+				elsif params[:raws] != ["0"] && params[:collection_ids] == ["0"]
+					collections_id = [nil]
+				elsif params[:raws] != ["0"] && params[:collection_ids] != ["0"]
+					collections_id = params[:collection_ids].push(nil)
 				else
-					msgs_collection = Message.order('id desc').where(collection_id: params[:collection_ids])
+					collections_id = ["0"]
 				end
 			end
 
+			msgs = Message.order('id desc').where(collection_id: collections_id)
 
-			if phone_messages
-				if msgs_collection
-					msgs = phone_messages & msgs_collection
-				else
-					msgs = phone_messages
-				end
+			if params[:phone_number]
+				phone_number = "%#{params[:phone_number]}%"
+				msgs = msgs.where('`from` like ?', phone_number)
 			end
 
-			if msgs_collection
-				if phone_messages
-					msgs = phone_messages & msgs_collection
-				end
-			end
-
-			if msgs
-				msgs = all_messages & msgs
-			else
-				msgs = all_messages
-			end
-
-			msgs.sort_by &:created_at
-
-			if params[:before_id]
-				msgs = msgs[params[:before_id].to_i..size+params[:before_id].to_i] if params[:before_id].to_i > 0
-			else
-				msgs = msgs[0..size]
-			end
-
-			# debugger
-			# msgs = msgs.limit(25)
 			activities_json = msgs.map do |message|
 				collection_name = message.collection_id.nil? ? "" : message.collection.name
 				{
