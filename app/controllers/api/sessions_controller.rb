da700133d065afdbd3ccf9746264c3d38add87a1
mann <chanmann@instedd.org>
2014-03-19 00:18:37 +0700

    check user confirmation when login

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,32 +1,39 @@
 class Api::SessionsController < Devise::SessionsController
-  before_filter :check_params, only: :create
+  before_filter :check_params, :login_attempt, only: :create
   skip_before_filter :require_no_authentication
 
+  ERRORS = {
+    invalid: 'Error with your login or password.',
+    invalid_token: 'Invalid authentication token.',
+    unconfirmed: 'You have to confirm your account before continuing.'
+  }
+
   def create
-    user = User.find_for_database_authentication email: params[:user][:email]
-    if user && user.valid_password?(params[:user][:password])
-      render json: { success: true, auth_token: user.authentication_token }, status: :created
-    else
-      invalid_login_attempt
-    end
+    render json: { success: true, auth_token: resource.authentication_token }, status: :created
   end
 
   def destroy
     user = User.find_by_authentication_token params[:auth_token]
-    if user && user.reset_authentication_token!
-      render :json => { }, :success => true, :status => 204
-    else
-      render :json => { :message => 'Invalid token.' }, :status => 404
-    end
+    return invalid_attempt :invalid_token, :not_found unless user
+
+    render json: { success: user.reset_authentication_token! }, status: :no_content
   end
 
   protected
-    def invalid_login_attempt 
-      warden.custom_failure!
-      render json: { success: false, message: 'Error with your login or password' }, status: 401
+    def login_attempt
+      self.resource = User.find_for_database_authentication email: params[:user][:email]
+
+      return invalid_attempt :invalid, :unauthorized unless resource
+      return invalid_attempt :unconfirmed, :unauthorized unless resource.active_for_authentication?
+      return invalid_attempt :invalid, :unauthorized unless resource.valid_password? params[:user][:password]
     end
 
     def check_params
-      return invalid_login_attempt unless params[:user]
+      return invalid_attempt :invalid, :unauthorized unless params[:user]
+    end
+
+    def invalid_attempt reason, status
+      warden.custom_failure!
+      render json: { success: false, message: ERRORS[reason] }, status: status
     end
 end
