4352262e5186eba7fa44f54702dc39d7b0b357c7
mann <chanmann@instedd.org>
2014-03-19 14:56:48 +0700

    use POST instead of DELETE for logout api

--- config/routes.rb
+++ config/routes.rb
@@ -126,7 +126,7 @@ ResourceMap::Application.routes.draw do
     devise_scope :user do
       post '/users' => 'registrations#create'
       post '/users/sign_in' => 'sessions#create'
-      delete '/users/sign_out' => 'sessions#destroy'
+      post '/users/sign_out' => 'sessions#destroy'
     end
 
     # v1
