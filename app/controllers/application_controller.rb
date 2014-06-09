6b61c519dda69facc269fba2d67c4c64a8b92abf
mann <chanmann@instedd.org>
2014-06-09 15:51:41 +0700

    let devise take care of token authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -76,8 +76,7 @@ class ApplicationController < ActionController::Base
   end
 
   def authenticate_api_user!
-    user = User.find_by_authentication_token(params.delete :auth_token) || current_user
-    head :forbidden unless user
+    head :forbidden unless current_user
   end
 
   def authenticate_collection_user!
