4eea46ed30eb9503f6f593087163ee32a7228efc
mann <chanmann@instedd.org>
2014-06-10 11:43:58 +0700

    fix api authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -76,8 +76,8 @@ class ApplicationController < ActionController::Base
   end
 
   def authenticate_api_user!
-    user = User.find_by_authentication_token(params.delete :auth_token) and sign_in user
-    head :forbidden unless user
+    params.delete :auth_token if current_user
+    head :forbidden unless current_user
   end
 
   def authenticate_collection_user!
