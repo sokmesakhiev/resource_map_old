61168f32f4e97143c872f3a3949bb2d42dea686f
mouylenglim <mouyleng@instedd.org>
2014-10-30 14:54:40 +0700

    fix x of undefined in offline mode in page create site

--- app/assets/javascripts/mobile/collections/collection.js
+++ app/assets/javascripts/mobile/collections/collection.js
@@ -569,16 +569,16 @@ Collection.prototype.applyBrowserLocation = function(){
 Collection.prototype.showError = function(error){
   switch(error.code){
     case error.PERMISSION_DENIED:
-      x.innerHTML="User denied the request for Geolocation."
+      Collection.prototype.showErrorMessage("User denied the request for Geolocation.")
       break;
     case error.POSITION_UNAVAILABLE:
-      x.innerHTML="Location information is unavailable."
+      Collection.prototype.showErrorMessage("Location information is unavailable.")
       break;
     case error.TIMEOUT:
-      x.innerHTML="The request to get user location timed out."
+      Collection.prototype.showErrorMessage("The request to get user location timed out.")
       break;
     case error.UNKNOWN_ERROR:
-      x.innerHTML="An unknown error occurred."
+      Collection.prototype.showErrorMessage("An unknown error occurred.")
       break;
   }
 }
