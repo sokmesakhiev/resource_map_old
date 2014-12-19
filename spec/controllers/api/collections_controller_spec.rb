590d7f007ee46800b4846e04fa60d03bdd5ac141
Sokmesa KHIEV <ksokmesa>
2014-12-19 15:35:44 +0700

    add some code to generate layer and properties in test

--- spec/controllers/api/collections_controller_spec.rb
+++ spec/controllers/api/collections_controller_spec.rb
@@ -162,9 +162,9 @@ describe Api::CollectionsController do
 
       csv[0].should eq(['resmap-id', 'name', 'lat', 'long', text.code, numeric.code, yes_no.code, select_one.code, select_many.code, hierarchy.code, site_ref.code, date.code, director.code, 'last updated'])
 
-      csv.should include [site2.id.to_s, site2.name, site2.lat.to_s, site2.lng.to_s, "", "", "no", "", "", "Bro", "", "", "", site2.updated_at.to_datetime.rfc822]
+      csv.include?([site2.id.to_s, site2.name, site2.lat.to_s, site2.lng.to_s, "", "", "no", "", "", "Bro", "", "", "", site2.updated_at.to_datetime.rfc822]).should eq(true)
 
-      csv.should include [site.id.to_s, site.name, site.lat.to_s, site.lng.to_s, site.properties[text.es_code], site.properties[numeric.es_code].to_s, 'yes', 'one', 'one, two', 'Dad', site2.id.to_s, '10/24/2012', user.email, site.updated_at.to_datetime.rfc822]
+      csv.include?([site.id.to_s, site.name, site.lat.to_s, site.lng.to_s, site.properties[text.es_code], site.properties[numeric.es_code].to_s, 'yes', 'one', 'one, two', 'Dad', site2.id.to_s, '10/24/2012', user.email, site.updated_at.to_datetime.rfc822]).should eq(true)
     end
   end
 
--- spec/controllers/api/sites_controller_spec.rb
+++ spec/controllers/api/sites_controller_spec.rb
@@ -6,6 +6,20 @@ describe Api::SitesController do
   let!(:user) { User.make }
   let!(:collection) { user.create_collection(Collection.make_unsaved) }
   let!(:collection2) { user.create_collection(Collection.make_unsaved) }
+
+  let!(:layer) { collection.layers.make }
+
+  let!(:text) { layer.text_fields.make :code => 'text'}
+  let!(:numeric) { layer.numeric_fields.make :code => 'numeric' }
+  let!(:yes_no) { layer.yes_no_fields.make :code => 'yes_no'}
+  let!(:select_one) { layer.select_one_fields.make :code => 'select_one', :config => {'options' => [{'id' => 1, 'code' => 'one', 'label' => 'One'}, {'id' => 2, 'code' => 'two', 'label' => 'Two'}]} }
+  let!(:select_many) { layer.select_many_fields.make :code => 'select_many', :config => {'options' => [{'id' => 1, 'code' => 'one', 'label' => 'One'}, {'id' => 2, 'code' => 'two', 'label' => 'Two'}]} }
+  config_hierarchy = [{ id: 'dad', name: 'Dad', sub: [{id: 'son', name: 'Son'}, {id: 'bro', name: 'Bro'}]}]
+  let!(:hierarchy) { layer.hierarchy_fields.make :code => 'hierarchy',  config: { hierarchy: config_hierarchy }.with_indifferent_access }
+  let!(:site_ref) { layer.site_fields.make :code => 'site' }
+  let!(:date) { layer.date_fields.make :code => 'date' }
+  let!(:director) { layer.user_fields.make :code => 'user'}
+  
   let!(:site) { collection.sites.make }
   let!(:site1) { collection.sites.make }
 
@@ -98,7 +112,7 @@ describe Api::SitesController do
       collection.sites.length.should eq(2)
     end
 
-    it "should save site in the collection" do
+    it "should delete site in the collection" do
       delete :destroy, format: 'json', id: site.id, collection_id: collection.id
       response.should be_success
       collection.sites.length.should eq(1)
