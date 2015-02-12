c78e115ae7776365b1ac6bcd5cb921a11086266a
thyda_eng <thyda@instedd.com>
2015-02-12 15:06:46 +0700

    change activity description to log on view activity

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -34,6 +34,7 @@ class ActivitiesController < ApplicationController
             collection: activity.collection.name,
             user: activity.user.display_name,
             description: activity.description,
+            log: activity.log,
             created_at: activity.created_at
           }
         end
