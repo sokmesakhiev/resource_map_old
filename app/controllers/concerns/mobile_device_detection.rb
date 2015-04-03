89a2a1bf1771212863e1c6d800c00e9d936cb8fc
mouylenglim <mouyleng@instedd.org>
2015-04-03 15:13:58 +0700

    remove log and add link login page

--- app/controllers/concerns/mobile_device_detection.rb
+++ app/controllers/concerns/mobile_device_detection.rb
@@ -19,14 +19,6 @@ module Concerns::MobileDeviceDetection
 
   def prepare_for_mobile
     session[:mobile_param] = params[:mobile] if params[:mobile]
-    # if params[:mobile]
-    #   session[:mobile_param] = params[:mobile]
-    #   session[:desktop_param] = 0
-    # end
-    # if params[:_desktop]
-    #   session[:desktop_param] = params[:_desktop]
-    #   session[:mobile_param] = 0
-    # end
     request.format = :mobile if mobile_device?
   end
 end
