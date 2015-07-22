c60847014963364b56c622d38335a268baf13a3f
Mouyleng LIM <mouyleng@instedd.org>
2015-07-22 06:09:03 +0000

    20150722040938_redelete_field_parent_from_collection.rb deleted online with Bitbucket

--- db/migrate/20150722040938_redelete_field_parent_from_collection.rb
+++
@@ -1,9 +0,0 @@
-class RedeleteFieldParentFromCollection < ActiveRecord::Migration
-  def up
-    remove_column :collections, :field_parent
-  end
-
-  def down
-    add_column :collections, :field_parent, :int
-  end
-end
