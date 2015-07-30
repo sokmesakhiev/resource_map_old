210d755e370b9a50c4257576d97a0cd7b1771cfd
thyda_eng <thyda@instedd.org>
2015-07-30 10:39:26 +0700

    change config guisso to local

--- db/schema.rb
+++ db/schema.rb
@@ -20,8 +20,8 @@ ActiveRecord::Schema.define() do
     t.integer  "field_id"
     t.integer  "site_id"
     t.binary   "data",            limit: 2147483647
-    t.datetime "created_at",                         null: false
-    t.datetime "updated_at",                         null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "item_type"
     t.string   "action"
     t.text     "log"
@@ -33,8 +33,10 @@ ActiveRecord::Schema.define() do
     t.string   "name"
     t.boolean  "is_enable"
     t.string   "password"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.boolean  "is_share"
+    t.text     "share_collections"
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "user_id"
     t.boolean  "basic_setup"
     t.boolean  "advanced_setup"
@@ -45,8 +47,8 @@ ActiveRecord::Schema.define() do
     t.string   "name"
     t.text     "description"
     t.boolean  "public"
-    t.datetime "created_at",                                                  null: false
-    t.datetime "updated_at",                                                  null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.decimal  "lat",                 precision: 10, scale: 6
     t.decimal  "lng",                 precision: 10, scale: 6
     t.decimal  "min_lat",             precision: 10, scale: 6
@@ -65,8 +67,8 @@ ActiveRecord::Schema.define() do
     t.string   "name"
     t.string   "code"
     t.string   "kind"
-    t.datetime "created_at",                       null: false
-    t.datetime "updated_at",                       null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.binary   "config",        limit: 2147483647
     t.integer  "ord"
     t.datetime "valid_since"
@@ -83,8 +85,8 @@ ActiveRecord::Schema.define() do
     t.string   "name"
     t.string   "code"
     t.string   "kind"
-    t.datetime "created_at",                                               null: false
-    t.datetime "updated_at",                                               null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.binary   "config",                limit: 2147483647
     t.integer  "ord"
     t.text     "metadata"
@@ -105,8 +107,8 @@ ActiveRecord::Schema.define() do
     t.string   "status"
     t.string   "original_filename"
     t.datetime "finished_at"
-    t.datetime "created_at",        null: false
-    t.datetime "updated_at",        null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "user_id"
     t.integer  "collection_id"
     t.text     "exception"
@@ -115,16 +117,16 @@ ActiveRecord::Schema.define() do
   create_table "languages", force: true do |t|
     t.string   "name"
     t.string   "code"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "layer_histories", force: true do |t|
     t.integer  "collection_id"
     t.string   "name"
     t.boolean  "public"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "ord"
     t.datetime "valid_since"
     t.datetime "valid_to"
@@ -139,31 +141,31 @@ ActiveRecord::Schema.define() do
     t.integer  "layer_id"
     t.boolean  "read",          default: false
     t.boolean  "write",         default: false
-    t.datetime "created_at",                    null: false
-    t.datetime "updated_at",                    null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "layers", force: true do |t|
     t.integer  "collection_id"
     t.string   "name"
     t.boolean  "public"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "ord"
   end
 
   create_table "login_failed_trackers", force: true do |t|
     t.datetime "login_at"
     t.string   "ip_address"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "memberships", force: true do |t|
     t.integer  "user_id"
     t.integer  "collection_id"
-    t.datetime "created_at",                    null: false
-    t.datetime "updated_at",                    null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.boolean  "admin",         default: false
     t.boolean  "owner",         default: false
   end
@@ -178,8 +180,8 @@ ActiveRecord::Schema.define() do
     t.string   "to"
     t.string   "subject"
     t.string   "body"
-    t.datetime "created_at",                    null: false
-    t.datetime "updated_at",                    null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.text     "reply"
     t.integer  "collection_id"
     t.boolean  "is_send",       default: false
@@ -187,8 +189,8 @@ ActiveRecord::Schema.define() do
 
   create_table "prefixes", force: true do |t|
     t.string   "version"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "reminders", force: true do |t|
@@ -196,8 +198,8 @@ ActiveRecord::Schema.define() do
     t.text     "reminder_message"
     t.integer  "repeat_id"
     t.integer  "collection_id"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.boolean  "is_all_site"
     t.text     "schedule"
     t.datetime "next_run"
@@ -212,16 +214,16 @@ ActiveRecord::Schema.define() do
   create_table "repeats", force: true do |t|
     t.string   "name"
     t.integer  "order"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.text     "rule"
   end
 
   create_table "share_channels", force: true do |t|
     t.integer  "channel_id"
     t.integer  "collection_id"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "site_histories", force: true do |t|
@@ -231,8 +233,8 @@ ActiveRecord::Schema.define() do
     t.decimal  "lng",                       precision: 10, scale: 6
     t.integer  "parent_id"
     t.string   "hierarchy"
-    t.datetime "created_at",                                                               null: false
-    t.datetime "updated_at",                                                               null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.text     "properties"
     t.string   "location_mode",  limit: 10,                          default: "automatic"
     t.string   "id_with_prefix"
@@ -247,8 +249,8 @@ ActiveRecord::Schema.define() do
   create_table "site_reminders", force: true do |t|
     t.integer  "reminder_id"
     t.integer  "site_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "site_reminders", ["reminder_id"], name: "index_site_reminders_on_reminder_id", using: :btree
@@ -261,8 +263,8 @@ ActiveRecord::Schema.define() do
     t.decimal  "lng",                       precision: 10, scale: 6
     t.integer  "parent_id"
     t.string   "hierarchy"
-    t.datetime "created_at",                                                               null: false
-    t.datetime "updated_at",                                                               null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.text     "properties"
     t.string   "location_mode",  limit: 10,                          default: "automatic"
     t.string   "id_with_prefix"
@@ -276,16 +278,16 @@ ActiveRecord::Schema.define() do
     t.string   "type"
     t.boolean  "all_sites",     default: true
     t.text     "some_sites"
-    t.datetime "created_at",                   null: false
-    t.datetime "updated_at",                   null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "snapshots", force: true do |t|
     t.string   "name"
     t.datetime "date"
     t.integer  "collection_id"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "thresholds", force: true do |t|
@@ -293,8 +295,8 @@ ActiveRecord::Schema.define() do
     t.string   "color"
     t.text     "conditions"
     t.integer  "collection_id"
-    t.datetime "created_at",           null: false
-    t.datetime "updated_at",           null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "name"
     t.boolean  "is_all_site"
     t.text     "sites"
@@ -308,8 +310,8 @@ ActiveRecord::Schema.define() do
   create_table "user_snapshots", force: true do |t|
     t.integer  "user_id"
     t.integer  "snapshot_id"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "collection_id"
   end
 
@@ -324,8 +326,8 @@ ActiveRecord::Schema.define() do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "phone_number"
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
